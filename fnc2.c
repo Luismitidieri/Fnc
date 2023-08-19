5ca00819b54a06b323f851aafc0272fe3bca7f31
Luismitidieri <127801883+Luismitidieri@users.noreply.github.com>
2023-08-19 02:56:50 -0300

    Add files via upload

---
+++ fnc2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+float maior(float num1, float num2){
+	if(num1>num2)
+	return num1;
+	else 
+	return num2;
+	
+	
+	int main(){
+		
+		float x,y,m;
+		printf("digite um numero\n:");
+		scanf("%f",&x);
+		printf("digite um numero\n:");
+			scanf("%f",&y);
+		m = maior(x,y);
+		printf("Maior:%2.f",m);
+	}
+}
